9e9fb7654ff602d7fe09dc7bfe5db71630583311
mukul <mukulkm9891@gmail.com>
2026-02-04 18:46:04 +0530

    first commit

---
+++ auditorium-booking-backend/src/main/java/com/bvicam/auditorium/booking/AuditoriumBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bvicam.auditorium.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditoriumBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditoriumBookingBackendApplication.class, args);
+	}
+
+}
---
+++ auditorium-booking-backend/src/test/java/com/bvicam/auditorium/booking/AuditoriumBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvicam.auditorium.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditoriumBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
